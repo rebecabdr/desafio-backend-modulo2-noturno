c9015f1e8e582660bda49cbd8bdcc429ee994d57
Rebeca Rocha <rebecabdrocha@gmail.com>
2021-06-19 14:59:14 -0300

    GETs completos, sem rotas e controladores

--- cubos-checkout/index.js
+++ cubos-checkout/index.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const data = require('./banco/data.json');
 const bodyParser = require('body-parser');
-const roteador = require("./rotas");
 
 const app = express();
 
 app.use(express.json());
 
-// app.use(roteador);
+
+// GET listar produtos, com filtros ou sem.
 
 app.get('/produtos', (req, res) =>{
     const produtos = data.produtos;
-    const produtosEstoque = produtos.filter(x => {
-        if(x.estoque !== 0){
-            return x
-    }});
+    const produtosEstoque = produtos.filter(x => x.estoque !== 0);
     const query = req.query;
 
-
-    if (query.categoria){
-        console.log(query.categoria)
+    if(query.precoInicial && query.precoFinal && query.categoria){
+       const filtroPrecosECategoria = produtosEstoque.filter(x => x.preco >= query.precoInicial && x.preco <= query.precoFinal && x.categoria.toLowerCase() === query.categoria.toLowerCase());
+       res.json(filtroPrecosECategoria)
+    } else if(query.precoInicial && query.precoFinal){
+        const filtroPrecos = produtosEstoque.filter(x => x.preco >= query.precoInicial && x.preco <= query.precoFinal)
+        res.json(filtroPrecos)
+    } else if (query.categoria){
         const filtroCategoria = produtosEstoque.filter(x => x.categoria.toLowerCase() === query.categoria.toLowerCase());
         res.json(filtroCategoria)
     } else {
